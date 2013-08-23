12d52ff6c1855fedf871a6866705a2bbe1f3a01f
camiloribeiro <camilo@camiloribeiro.com>
2013-08-23 20:02:02 -0300

    Fatorial working

--- fatorial/fatorial.erl
+++ fatorial/fatorial.erl
@@ -1,4 +1,6 @@
 -module(fatorial).
 -compile(export_all).
 
-do_it(A) -> (A).
+do_it(0) -> 1;
+do_it(A) ->
+  A * do_it(A-1).
--- fatorial/fatorial_test.erl
+++ fatorial/fatorial_test.erl
@@ -3,4 +3,7 @@
 
 do_it_test() -> 
   ?assertEqual(1, fatorial:do_it(0)),
-  ?assertEqual(1, fatorial:do_it(1)).
+  ?assertEqual(1, fatorial:do_it(1)),
+  ?assertEqual(2, fatorial:do_it(2)),
+  ?assertEqual(6, fatorial:do_it(3)),
+  ?assertEqual(93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000, fatorial:do_it(100)).
