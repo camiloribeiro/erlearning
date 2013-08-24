d122741bd0daa69a05c7d074794600905c7b8083
camiloribeiro <camilo@camiloribeiro.com>
2013-08-24 01:44:31 -0300

    adding the ac to test cases

--- reverse_polish_calc/calc_test.erl
+++ reverse_polish_calc/calc_test.erl
@@ -8,4 +8,5 @@ run_test() ->
   ?assertEqual(4, calc:run([2,2,"*"])),
   ?assertEqual(2, calc:run([4,2,"/"])),
   ?assertEqual(4, calc:run([4,2,"/",2,"*"])),
+  ?assertEqual(23, calc:run([4,6,"+",5,"*",2,"/",2,"-"])),
   ?assertEqual(150, calc:run([4,2,"/",2,"*",4,"-",15,"+",10,"*"])).
