7ebaad5c5c79ac433b3f011520e6526b5992998d
camiloribeiro <camilo@camiloribeiro.com>
2013-09-03 18:47:33 -0300

    adding new functions

--- euler/p3/problem3.erl
+++ euler/p3/problem3.erl
@@ -1,5 +1,20 @@
 -module(problem3).
 -compile(export_all).
 
-factorize(Max) ->
-  [2,3,5].
+factorize(Number) ->  
+  primes(Number, max_primes([Number])).
+primes(Num,[H|T]) ->
+  if 
+    Num rem H == 0 -> H;
+    true -> primes(Num, T)
+  end.
+max_primes([H|T]) ->
+  if H > 1 ->
+    max_primes([H - 1|[H|T]]);
+    true -> [H|T]
+  end.
+is_prime(Num, [H|T]) ->
+  if 
+    Num rem H == 0 -> false;
+    true -> is_prime(Num, T)
+  end.
