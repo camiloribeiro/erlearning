54cdc06386d066c567959f73ee51463e80329f7e
camiloribeiro <camilo@camiloribeiro.com>
2013-09-17 16:14:41 -0300

    Rethinkning the whole thing

--- euler/p3/problem3.erl
+++ euler/p3/problem3.erl
@@ -1,72 +1,11 @@
 -module(problem3).
 -compile(export_all).
 
-factorize(Number) ->  
-  List = primes(round(math:sqrt(Number))),
-  factorize(List, Number).
-
-factorize([], Number) ->  
-  round(Number / Number);
-
-factorize([H|T], Number) ->  
-  if
-    Number rem H == 0 -> H;
-    true -> factorize(T, Number)
-  end.
-
-primes(Num) ->
-  List = create_list(Num),
-  primes(List, []).
-
-primes([], New_list) ->
-  New_list;
-
-primes([H|T], New_list) ->
-  Foo = is_prime(H),
-  if 
-    Foo == 0 ->
-      primes(T, New_list);
-    T /= [] ->
-      primes(T, [Foo|New_list]);
-    true -> New_list
-  end.
-
-is_prime(2) ->
-  2;
-
-is_prime(Num) ->
-  List = create_list(Num - 1),
-  is_prime(Num, List).
-
-is_prime(Num, [H|T]) ->
-  if 
-    T == [] -> Num;
-    Num rem H /= 0 -> is_prime(Num, T);
-    true -> 0
-  end.
-
 create_list(Num) ->
   create_list([],Num).
 
-create_list(List, 1) ->
-  [2|List];
-  %create_list([2|List], 0);
-
-create_list(List, 0) ->
-  [1|List];
-
-create_list(List, Num) ->
-  Foo = return_odd_num(Num),
+create_list(List,Num) ->
   if 
-    Foo > 1 ->
-      create_list([Foo|List], Num - 1);
-      true -> 
-        create_list(List, Num - 1)
-    end.
-
-return_odd_num(Num) ->
-  if 
-    Num rem 2 /= 0 ->
-      Num;
-    true -> 0
+    (Num /= 0) -> create_list([Num|List], Num - 1);
+    true -> List
   end.
--- euler/p3/problem3_test.erl
+++ euler/p3/problem3_test.erl
@@ -2,30 +2,4 @@
 -include_lib("eunit/include/eunit.hrl").
 
 create_list_test() ->
-  ?assertEqual([2,3,5,7,9], problem3:create_list(10)).
-
-return_odd_num_test() ->
-  ?assertEqual(0, problem3:return_odd_num(10)),
-  ?assertEqual(11, problem3:return_odd_num(11)).
-
-is_prime_test() ->
-  ?assertEqual(2,  problem3:is_prime(2)), 
-  ?assertEqual(3,  problem3:is_prime(3)), 
-  ?assertEqual(0, problem3:is_prime(4)), 
-  ?assertEqual(5,  problem3:is_prime(5)), 
-  ?assertEqual(0, problem3:is_prime(6)), 
-  ?assertEqual(7,  problem3:is_prime(7)), 
-  ?assertEqual(0, problem3:is_prime(8)), 
-  ?assertEqual(0, problem3:is_prime(9)), 
-  ?assertEqual(0, problem3:is_prime(10)),
-  ?assertEqual(11,  problem3:is_prime(11)).
-
-factorize_test() ->
-  ?assertEqual(1, problem3:factorize(3)),
-  ?assertEqual(5, problem3:factorize(360)),
-  ?assertEqual(29, problem3:factorize(13195)).
-
-primes_test() ->
-  ?assertEqual([7,5,3,2], problem3:primes(10)),
-  ?assertEqual([97,89,83,79,73,71,67,61,59,53,47,43,41,37,31,29,23,19,17,13,11,7,5,3,2], problem3:primes(100)).
-
+  ?assertEqual([1,2,3,4,5,6,7,8,9,10], problem3:create_list(10)).
