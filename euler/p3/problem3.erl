407c24110dfa5880d302d27cf0b5ba08ed2a164d
camiloribeiro <camilo@camiloribeiro.com>
2013-09-17 19:36:41 -0300

    first iteration working ok

--- euler/p3/problem3.erl
+++ euler/p3/problem3.erl
@@ -9,3 +9,16 @@ create_list(List,Num) ->
     (Num /= 1) -> create_list([Num|List], Num - 1);
     true -> List
   end.
+
+primes([H|T]) ->
+  primes(H, T, [H]).
+
+primes(Num, [], New_list) ->
+  lists:reverse(New_list);
+
+primes(Num, [H|T], New_list) ->
+  if 
+    H rem Num == 0 -> primes(Num, T, New_list);
+    H rem Num /= 0 -> primes(Num, T, [H|New_list]);
+    true -> New_list
+  end.
--- euler/p3/problem3_test.erl
+++ euler/p3/problem3_test.erl
@@ -3,3 +3,6 @@
 
 create_list_test() ->
   ?assertEqual([2,3,4,5,6,7,8,9,10], problem3:create_list(10)).
+
+primes_test() ->
+  ?assertEqual([2,3,5,7,9,11,13,15,17,19], problem3:primes([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19])).
