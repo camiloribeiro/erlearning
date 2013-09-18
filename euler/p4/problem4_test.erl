cd971d5bad04ece1ede1a9bb4727dc2948282f48
camiloribeiro <camilo@camiloribeiro.com>
2013-09-18 17:08:46 -0300

    adding tests for problem 4

---
+++ euler/p4/problem4_test.erl
@@ -0,0 +1,16 @@
+-module(problem4_test).
+-include_lib("eunit/include/eunit.hrl").
+
+get_palindromic_test() ->
+  ?assertEqual(9009, problem4:get_palindromic(99)).
+
+
+is_palindromic_test() ->
+  ?assertEqual(true, problem4:is_palindromic(3)),
+  ?assertEqual(true, problem4:is_palindromic(99)),
+  ?assertEqual(false, problem4:is_palindromic(90)),
+  ?assertEqual(true, problem4:is_palindromic(999)),
+  ?assertEqual(true, problem4:is_palindromic(909)),
+  ?assertEqual(false, problem4:is_palindromic(900)),
+  ?assertEqual(true, problem4:is_palindromic(9009)),
+  ?assertEqual(false, problem4:is_palindromic(9090)).
