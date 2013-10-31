b385de9d56e9574999257d3e4f392391a98559ee
Camilo Ribeiro <camilo.ribeiro@klarna.com>
2013-10-31 10:07:32 +0100

    adding first test

--- euler/p8/problem8.erl
+++ euler/p8/problem8.erl
@@ -1,3 +1,4 @@
 -module(problem8).
 -compile(export_all).
 
+get_five(Num) -> Num.
--- euler/p8/problem8_test.erl
+++ euler/p8/problem8_test.erl
@@ -1,3 +1,5 @@
 -module(problem8_test).
 -include_lib("eunit/include/eunit.hrl").
 
+get_five_test() ->
+  ?assertEqual(12345, problem8:get_five(123456)).
