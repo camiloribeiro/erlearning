94b50e32c2ad316903221b1f840502681ec2a2c1
Camilo Ribeiro <camilo.ribeiro@klarna.com>
2013-10-31 10:40:18 +0100

    get bigger number working

--- euler/p8/problem8.erl
+++ euler/p8/problem8.erl
@@ -1,4 +1,17 @@
 -module(problem8).
 -compile(export_all).
 
-get_five(Num) -> string:substr(Num, 1, 5).
+get_bigger(Num) -> 
+  get_bigger(Num, 1, string:len(Num) - 4, 0). 
+    
+    
+get_bigger(Num, Position, Max, Bigger) -> 
+  if 
+    (Position > Max) -> Bigger;
+    true -> 
+        Current  = string:substr(Num, Position , 5), 
+      if 
+        (Bigger > Current) -> get_bigger(Num, Position + 1, Max, Bigger);
+        true -> get_bigger(Num, Position + 1, Max, Current)
+      end
+  end.
