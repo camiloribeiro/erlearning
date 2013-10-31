c19510a7c36a72defb34359fb47b667809c91877
Camilo Ribeiro <camilo.ribeiro@klarna.com>
2013-10-31 19:48:29 +0100

    changing test to run the final function

--- euler/p9/problem9_test.erl
+++ euler/p9/problem9_test.erl
@@ -14,7 +14,7 @@ run_list_test() ->
   ?assertEqual([3,4,5], problem9:run_list([[5,6,4],[3,4,5],[1,3,6],[1,2,7]])).
 
 get_pythagorean_test() ->
-  ?assertEqual([3,4,5], problem9:get_pythagorean(12)).
+  ?assertEqual(60, problem9:get_pythagorean(12)).
 
 get_product_test() ->
   ?assertEqual(60, problem9:get_product([3,4,5])).
